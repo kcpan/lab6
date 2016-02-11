e4aec5954433108d49c1d1962d587bdbb02a8ba4
Kevin Pan <kcpan@ucsd.edu>
2016-02-11 03:47:56 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,23 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	var url = "/project/"+idNumber;
+	console.log(url);
+	$.get(url, addProject);
+
+}
+
+function addProject(result){
+	console.log(result);
+	var projectHTML=
+		'<img src="' + result['image'] + '" class="detailsImage">' +
+		'<p>' + result['title'] + '</p>' +
+		'<p><small>' + result['date']+'</small></p></a>' +
+		'<p>' + result['summary']+'</p>';
+  var select = '#project'+result['id']+' .details';
+	$(select).html(	projectHTML );
+
 }
 
 /*
@@ -35,4 +52,15 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette", setPalette);
+}
+
+function setPalette(result) {
+	console.log(result);
+	var colors = (result['colors'])['hex'];
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', '.75');
 }
